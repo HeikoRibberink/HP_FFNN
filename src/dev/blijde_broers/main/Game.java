3d0fc9748da978308f31b2692cc469be515d9e88
HeikoRibberink <47252922+HeikoRibberink@users.noreply.github.com>
2020-01-30 15:08:47 +0100

    HEEELP

--- src/dev/blijde_broers/main/Game.java
+++ src/dev/blijde_broers/main/Game.java
@@ -125,12 +125,6 @@ public class Game implements Runnable {
 		}
 		g.setColor(Color.white);
 		if (trainer.out != null) {
-			for (int i = 0; i < trainer.out.length; i++) {
-				g.drawString(Double.toString(Math.round(trainer.out[i] * 100)), 500, 100 + (i * 50));
-			}
-			for (int i = 0; i < trainer.correctAnswers.length; i++) {
-				g.drawString(Double.toString(Math.round(trainer.correctAnswers[i] * 100)), 600, 100 + (i * 50));
-			}
 			double[] out = trainer.out;
 			int highestValueID = 0;
 			for(int i = 0; i < 10; i++) {
